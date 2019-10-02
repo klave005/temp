f2f9bf9902650fba75e03266559433274aeff3d9
Kyle Lavelle <klave005@wch136-30.cs.ucr.edu>
2019-10-02 10:51:33 -0700

    test

---
+++ 2/lab2/source/main.c
@@ -0,0 +1,27 @@
+/*	Author: klave005
+ *  Partner(s) Name: quinterra
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif	
+
+int main(void) {
+DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
+				// Initialize output on PORTB to 0x00
+unsigned char temp_input = 0x00;
+while(1) {
+	temp_input = PINA;
+	PORTB = temp_input;	// Writes port B's 8 pins with the values
+				// on port A's 8 pins
+}
+return 1;
+}
+
